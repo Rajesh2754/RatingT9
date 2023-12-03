6e70d3c093748879e734b3e8f655f82aadf0bca2
Rajesh <rajesh.ramakrishnan@walmart.com>
2023-12-04 02:46:22 +0530

    all games played

--- src/main/java/com/walmart/ecartReviews/EcartReviewsApplication.java
+++ src/main/java/com/walmart/ecartReviews/EcartReviewsApplication.java
@@ -4,11 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
 
 @SpringBootApplication
-
 public class EcartReviewsApplication {
 	private static final Logger logger = LoggerFactory.getLogger(EcartReviewsApplication.class);
 
